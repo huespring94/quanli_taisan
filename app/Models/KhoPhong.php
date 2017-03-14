6b8408f126d464a454ac06c17d6c4181910a70d9
huespring94 <huespringnt@gmail.com>
2017-03-14 20:49:22 +0700

    add

--- app/Models/KhoPhong.php
+++ app/Models/KhoPhong.php
@@ -8,6 +8,9 @@ class KhoPhong extends Model
 {
     protected $table = 'kho_phong';
     
+    const TT_MOI = 'moi';
+    const TT_DA_SU_DUNG = 'da su dung';
+    
     protected $fillable = [
         'ma',
         'ngay_nhap',

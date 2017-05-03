00981f64bb1a6d63df8c58c24e7881bd04744ce9
at-huenguyen <hue.nguyen@asiantech.vn>
2017-05-04 00:48:17 +0700

    ui by role

--- config/constant.php
+++ config/constant.php
@@ -3,6 +3,10 @@
 return [
   
     'rate_deadline' => 10,
-    
+    'r_admin' => \App\Models\Role::ROLE_ADMIN,
+    'r_accountant' => \App\Models\Role::ROLE_ACCOUNTANT,
+    'r_faculty' => \App\Models\Role::ROLE_FACULTY,
+    'r_room' => \App\Models\Role::ROLE_ROOM,
+    'r_user' => \App\Models\Role::ROLE_USER,
 ];
 
